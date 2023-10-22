8a67706b35fa88ae27896c7efe037a49e3829ec7
Sean Chen <tix123@msn.com>
2023-10-21 23:37:50 -0600

    update

---
+++ 20231021.js
@@ -0,0 +1,11 @@
+
+
+if (Number(lines[0]) >= 1000 ){
+    lines[0] = Number(lines[0])-100
+} 
+
+lines[0] = (Number(lines[0])+ Number(lines[1]))*2
+
+lines[0] = Number(lines[0])* Number(lines[0])
+
+lines[0] = Number(lines[0])* 13
